8c8380b6b4093eeaf06b8286f8a3a8757f4e383c
daiki905 <daikidenanika@yahoo.co.jp>
2025-02-03 22:23:43 +0900

    データベース設計２
    This reverts commit 58ab6b7a4b1d1d10fb07607eeb5a65f9d0a3bef7.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,2 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth
-
-
-  private
-
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == 'daiki' && password == '5062'
-    end
-  end
 end
