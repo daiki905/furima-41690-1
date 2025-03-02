3ce3e76eaa170bed8debb4791f81f60a042e6ccc
daiki905 <daikidenanika@yahoo.co.jp>
2025-03-02 21:45:45 +0900

    商品出品機能５

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,11 +4,11 @@ class Item < ApplicationRecord
   validates :item_name, presence: true
   validates :item_money, presence: true,numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :item_manual, presence: true
+  validates :item_manual, numericality: { other_than: 1 , message: "can't be blank"} 
   validates :item_status_id,numericality: { other_than: 1 , message: "can't be blank"} 
-  validates :payee_id,presence: true
-  validates :shippingsource_id,presence: true 
-  validates :readtime_id,presence: true
+  validates :payee_id,numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :shippingsource_id,numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :readtime_id,numericality: { other_than: 1 , message: "can't be blank"} 
 
   belongs_to :user
 
