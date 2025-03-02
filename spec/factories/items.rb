9d8741275d5d2dc53064c16457a1ad688de4628b
daiki905 <daikidenanika@yahoo.co.jp>
2025-03-02 21:33:32 +0900

    商品出品機能４

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,16 @@
 FactoryBot.define do
   factory :item do
-    
+    item_name        { '太郎' }
+    item_money       { 500 }
+    item_manual      { '山田' }
+    category_id      { 2 }
+    item_status_id   { 3 }
+    payee_id         { 2 }
+    shippingsource_id { 2 }
+    readtime_id      { 2 }
+    association :user
+    after(:build) do |item|
+      item.image.attach(io: File.open('spec/fixtures/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
