da7fd3b0347ec4eded9fc069acfef12a3854605d
daiki905 <daikidenanika@yahoo.co.jp>
2025-03-08 19:05:45 +0900

    商品情報編集機能１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,9 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
+  before_action :find_item, only: [:edit, :update]
+  before_action :authenticate_user!, only: [:edit, :update]
+  before_action :ensure_owner, only: [:edit, :update]
+
   def index
     @items = Item.order(created_at: :desc)
   end
@@ -23,7 +27,43 @@ class ItemsController < ApplicationController
     @user = @item.user
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to @item, notice: '商品情報が更新されました。'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  
+
   private
+
+  
+  def ensure_owner
+    @item = Item.find(params[:id])
+    unless @item.user_id == current_user.id
+      redirect_to root_path, notice: 'このページにはアクセスできません。'
+    end
+  end
+
+  def find_item
+    @item = Item.find_by(id: params[:id])
+    unless @item
+      redirect_to root_path, alert: '商品が見つかりません。'
+      return
+    end
+  end
+
+  def redirect_if_not_seller
+    if @item.nil? || @item.user != current_user
+      redirect_to root_path, alert: '他のユーザーの商品情報は編集できません。'
+    end
+  end
   
   def item_params
     params.require(:item).permit(:item_name, :item_money, :item_manual, :category_id, :item_status_id, :shippingsource_id, :payee_id, :readtime_id, :image).merge(user_id: current_user.id)
--- app/models/item.rb
+++ app/models/item.rb
@@ -5,7 +5,7 @@ class Item < ApplicationRecord
 
   
 
-  validates :item_name, presence: true
+  validates :item_name, presence: { message: "can't be blank" }
   validates :item_money, presence: true
   validates :item_money, numericality: { only_integer: true, greater_than_or_equal_to: 300,less_than_or_equal_to: 9999999, allow_nil: true, }
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
